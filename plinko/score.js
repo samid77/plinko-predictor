b528dc8669849c3beb9466f61daa3f4f657e0006
Samid77 <Dimas@Inass-Mac-Air.local>
2023-04-05 17:02:34 +0700

    Implement analysis logic using lodash

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,26 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel])
 }
 
-function runAnalysis() {
-  // Write code here to analyze stuff
+const distance = (point) => {}
+
+const runAnalysis = () => {
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value()
+
+    console.log(`bucket: ${bucket}`);
 }
 
